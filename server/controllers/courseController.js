d47c54e38035aa2b870c37a1541f39c1731afe83
Mahmoud-Ahmed-Awad <mahmodawadahmed@gmail.com>
2025-07-19 02:31:05 +0300

    add courseController

---
+++ server/controllers/courseController.js
@@ -0,0 +1,65 @@
+import Course from "../models/Course.js";
+
+// Get All Courses
+export const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ isPublished: true })
+      .select(["-courseContent", "-enrolledStudents"])
+      .populate({ path: "educator" });
+
+    res.status(200).json({
+      success: true,
+      courses,
+    });
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// Get Course by Id
+export const getCourseId = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const courseData = await Course.findById(id).populate({ path: "educator" });
+
+    // Check if course educator
+    const userId = req.auth().userId;
+
+    if (userId && userId.toString() === courseData.educator._id.toString()) {
+      return res
+        .status(200)
+        .json({ success: true, courseEducator: true, courseData });
+    }
+
+    // Remove lectureUrl if isPreviewFree is False
+    courseData.courseContent.forEach((chapter) => {
+      chapter.chapterContent.forEach((lecture) => {
+        if (!lecture.isFreePreview) {
+          lecture.lectureUrl = "";
+        }
+      });
+    });
+
+    res.status(200).json({
+      success: true,
+      courseData,
+    });
+  } catch (error) {
+    console.error("Error fetching course by ID:", error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// Get Courses With Learner Type
+export const coursesWithLearnerType = async (req, res) => {
+  try {
+    const { learnerType } = req.params;
+    const courses = await Course.find({ learnerType });
+    return res.status(200).json({ success: true, courses });
+  } catch (error) {
+    console.error("Error fetching courses by learner type:", error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
